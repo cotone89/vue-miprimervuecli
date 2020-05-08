3c53565b0e4fd4202aa65716b00f5c93f725e00b
Maria Jose Tamayo <ma.jose.tamayo89@gmail.com>
2020-05-08 16:55:29 -0400

    desafio ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1 class="text-center">{{titulo}}</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Run</th>
+                    <th scope="col">Nacimiento</th>
+                    <th scope="col">Edad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(usuario, index) of usuarios" v-bind:key="index">
+                    <th scope="row">{{index+1}}</th>
+                    <td>{{usuario.nombre}}</td>
+                    <td>{{usuario.apellido}}</td>
+                    <td>{{usuario.run}}</td>
+                    <td>{{usuario.nacimiento}}</td>
+                    <td>{{usuario.edad}}</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                titulo: 'Tabla de Usuarios',
+                usuarios: [{
+                    'nombre': 'nombre1',
+                    'apellido': 'apellido1',
+                    'run': 'run1',
+                    'nacimiento': 'nacimiento1',
+                    'edad': 'edad1'
+                },
+                {
+                    'nombre': 'nombre2',
+                    'apellido': 'apellido2',
+                    'run': 'run2',
+                    'nacimiento': 'nacimiento2',
+                    'edad': 'edad2'
+                },
+                {
+                    'nombre': 'nombre3',
+                    'apellido': 'apellido3',
+                    'run': 'run3',
+                    'nacimiento': 'nacimiento3',
+                    'edad': 'edad3'
+                }
+                ]
+            }
+        }
+    }
 </script>
 
 <style>
